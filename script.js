1db31db7c036ad03f89061defa332512d947d40c
obx88 <33556172+obx88@users.noreply.github.com>
2020-01-31 22:48:38 +0100

    test

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const fil0 = document.querySelectorAll('.fil0');
-const fil1 = document.querySelector('.fil1');
-const fil2 = document.querySelector('.fil2');
-const fil3 = document.querySelector('.fil3');
+const fil0 = document.querySelectorAll(".fil0");
+const fil1 = document.querySelector(".fil1");
+const fil2 = document.querySelector(".fil2");
+const fil3 = document.querySelector(".fil3");
 const fils = [fil0, fil2];
 let hue = 0;
 const generateColor = () => {
-  const color = `hsl(${hue},75%,35%)`;
-  fil0.forEach((fil) => (fil.style.fill = color));
-  hue += 10;
+  const color = `hsl(${hue},55%,25%)`;
+  fil0.forEach(fil => (fil.style.fill = color));
+  hue += 1;
 };
 setInterval(() => {
   generateColor();
